e5aa12cc60c864a4b143eab2f651c2e987f512af
Rafael Marçal <rafaelpsm@outlook.com.br>
2024-05-15 12:04:12 -0300

    Arquivos base do projeto

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        NomeVeiculo: 'Volkswagen Jetta',
+        Placa: 'ADQ-442S',
+        Seguro: 'Porto Seguro',
+        status: 'Pendente',
+    },
+    {
+        NomeVeiculo: 'Hyundai HB20',
+        Placa: 'XYZ-5678',
+        Seguro: 'Allianz',
+        status: 'Recusado',
+    },
+    {
+        NomeVeiculo: 'Renault Kwid',
+        Placa: 'DEF-9012',
+        Seguro: 'Tokio Marine',
+        status: 'Ativo',
+    },
+]
